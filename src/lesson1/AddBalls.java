d4b133f0135ee2ec4aed31c082cc350e24c3fa30
Alex <alexdolzenko@gmail.com>
2020-12-07 23:19:17 +0000

    первое дз

---
+++ src/lesson1/AddBalls.java
@@ -0,0 +1,30 @@
+package lesson1;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class AddBalls extends MouseAdapter {
+    private Timer timer;
+    MainCircles controller;
+
+    public AddBalls(MainCircles controller){
+        this.controller = controller;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        super.mouseClicked(e);
+        if(e.getButton()==MouseEvent.BUTTON1){
+            System.out.println("Ball is added");
+            controller.addBall();
+        } else if ((e.getButton()==MouseEvent.BUTTON3)){
+            System.out.println("Ball is removed");
+            controller.removeBall();
+        }
+
+    }
+
+
+}
+
